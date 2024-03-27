da5febeb05166bf1bac8dcf93885a5e4f0943377
Pedro Balla <105303512+MrBalla@users.noreply.github.com>
2024-03-27 20:10:00 +0100

    Update script.js

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -89,7 +89,7 @@ const handleSubmit = (event)=>{
     login.style.display="none";
     chat.style.display="flex";
     
-    websocket= new WebSocket("ws://localhost:8080");
+    websocket= new WebSocket("ws://chat-pyyy.onrender.com");
     websocket.onmessage = processMessage;
 
 }
